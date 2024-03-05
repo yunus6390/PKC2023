0971f15da324e2e580528bc13ed76f469f3ab593
yunus6390 <yunus.teli55@gmail.com>
2024-03-05 19:31:45 +0530

    push with vscode

---
+++ day_1/03_string.py
@@ -0,0 +1,11 @@
+# print("Hello World")
+
+# print('We are learning python with codanics')
+# print('''I am yunus.
+#       learning python with codanics
+#       this is day 3 of learning journey''')
+
+print("What's               up?"       )
+
+# print("2+3+6+9")
+# Assignment: what is type casting and do we use that in python
